183c04b807597200dd436743e495d53aa4639e6b
zlata <zlato4ka231105@mail.ru>
2022-06-09 12:14:43 +0300

    --all

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
+
